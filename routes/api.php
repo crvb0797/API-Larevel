1ceeb7959ba5d9ef32b2dffa523669fd29b24ff0
Carlos Villatoro <crvb0797@gmail.com>
2025-01-08 19:16:06 -0600

    Se configuro que la API sea privada

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // v1
-Route::apiResource('v1/posts', PostV1::class)->only(['index', 'show', 'destroy']);
+Route::apiResource('v1/posts', PostV1::class)->only(['index', 'show', 'destroy'])->middleware('auth:sanctum');
 
 // v2
-Route::apiResource('v2/posts', PostV2::class)->only(['index', 'show']);
+Route::apiResource('v2/posts', PostV2::class)->only(['index', 'show'])->middleware('auth:sanctum');
